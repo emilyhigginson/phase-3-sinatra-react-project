17dd4357b57f0b521db926ee35a38d6dce585de2
Gil <gcastillohtx@gmail.com>
2022-03-11 12:17:04 -0600

    gil

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -2,20 +2,29 @@ class SongController < ApplicationController
     set :default_content_type, 'application/json'
 
     #get all songs 
-    get '/songs' do 
-        songs = Song.all 
-        # songs = Song.where(day_id: 1) 
-        songs.to_json
+    # get '/songs' do 
+    #     songs = Song.where(user_id: 1])
+    #     # songs = Song.where(day_id: 1) 
+        
+    #     songs.to_json
+    # end
+
+    get '/all' do 
+        all = Song.all # only all songs for today
+
+        all.to_json(include: :user)
     end
+
+    #  get '/songs' do 
+    #     songs = Song.where(user_id: 3)
+    #     # songs = Song.where(day_id: 1) 
+    #     songs.to_json
+    # end
 
     #add a new song 
     post '/songs' do 
-        song = Song.create(
-            title: params[:title],
-            artist: params[:artist],
-            lyric: params[:lyric],
-            link: params[:link],
-        )
+        song = Song.create(params)
+
         song.to_json
     end
     
--- db/migrate/20220307221045_create_days.rb
+++ db/migrate/20220307221045_create_days.rb
@@ -5,3 +5,4 @@ class CreateDays < ActiveRecord::Migration[6.1]
     end
   end
 end
+ 
